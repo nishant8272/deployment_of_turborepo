19a82de525b75c24f11fb54ad5e2cf5e1d48e66c
nishant verma <nishantverma01042005@gmail.com>
2025-11-09 23:08:45 +0530

    Clean up <br> tags in page.tsx
    Removed unnecessary <br> tags from the page.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,13 +6,13 @@ export default  async function home() {
   return (
     <div>
       <h1> this the next app </h1>
-      <br></br>
+     
       <h2>name :</h2>
       {users?.name}
       <h2>
         Welcome to Next.js!
       </h2>
-      <br> </br>
+      
       <h1>
       email :</h1>
       {users?.email}
