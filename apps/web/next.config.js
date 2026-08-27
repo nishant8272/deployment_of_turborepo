1e2deb2b9d7eec90efda68cd35eb6c3f047d65ae
nishant8272 <nishantverma01042005@gmail.com>
2026-08-27 23:23:13 +0530

    fix9

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   turbopack: {
     root: "../../"
   },
-  serverExternalPackages: ["@prisma/client", "@repo/db"]
+  serverExternalPackages: ["@prisma/client", "@repo/db"],
+  output: "standalone",
 };
 
 export default nextConfig;
